9d0cbd909dae03cccf4d69986eddc34a7862b374
rebirthbtn24 <rebirthbtn@gmail.com>
2019-01-24 20:29:46 +0900

    ボタンがいない

---
+++ taskapp taskapp taskapp taskapp taskApp/Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  taskapp taskapp taskapp taskapp taskApp
+//
+//  Created by 増田弘毅 on 2019/01/24.
+//  Copyright © 2019 rebirthbtn24. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
