8a0a9ffdf48af258e61a5b2dd32cac3aea4bbeab
rebirthbtn24 <rebirthbtn@gmail.com>
2019-01-24 18:34:06 +0900

    pod installと.gitignoreの作成まで実施

---
+++ taskapp taskapp taskapp taskapp taskApp/InputViewController.swift
@@ -0,0 +1,34 @@
+//
+//  InputViewController.swift
+//  taskapp taskapp taskapp taskapp taskApp
+//
+//  Created by 増田弘毅 on 2019/01/21.
+//  Copyright © 2019 rebirthbtn24. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
